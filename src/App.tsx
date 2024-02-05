fa227cd87f2b40df6787c5a7c7b547acd226cecb
Ryan <mckevitt.r@gmail.com>
2024-02-04 19:44:00 -0600

    initialized NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Navbar from "./components/Navbar.tsx";
 
 function App() {
 
   return (
     <>
+        <Navbar/>
       <p>
           Website
       </p>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/NavBar.css";
+
+const NavBar: React.FC = () => {
+    return (
+        <div className="NavBar">
+            <p>NavBar</p>
+        </div>
+    );
+};
+
+export default NavBar;
